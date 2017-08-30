6989fdeb54f2acf8b02eaae4e78df42a2f03b084
Atılım Güneş Baydin <gbaydin@users.noreply.github.com>
2017-08-30 14:07:35 +0100

    Update project.clj

--- exercises/exercise-3-inference-compilation/project.clj
+++ exercises/exercise-3-inference-compilation/project.clj
@@ -7,5 +7,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [anglican "1.1.0-SNAPSHOT"]
-                 [anglican-infcomp "0.2.2-SNAPSHOT"]]
+                 [anglican-infcomp "0.2.3-SNAPSHOT"]]
   :main exercises.core)
